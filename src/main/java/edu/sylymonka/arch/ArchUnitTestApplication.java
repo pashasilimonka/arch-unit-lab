0f0691bdc3a27153c9bab7e45b0e8ce87a430a4e
pashasilimonka <125122642+pashasilimonka@users.noreply.github.com>
2024-03-08 00:47:34 +0200

    initial commit

---
+++ src/main/java/edu/sylymonka/arch/ArchUnitTestApplication.java
@@ -0,0 +1,13 @@
+package edu.sylymonka.arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchUnitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sylymonka/arch/ArchUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sylymonka.arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
